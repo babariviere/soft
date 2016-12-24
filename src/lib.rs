e9ce57b0d8c2bca78909e198948e104c5118a97c
babariviere <babariviere@protonmail.com>
2016-12-24 18:43:03 +0100

    More and more test

--- src/lib.rs
+++ src/lib.rs
@@ -57,8 +57,44 @@ mod tests {
         let server_thread = thread::spawn(move || {
             let (client, _) = server_stream.accept().unwrap();
             let mut server = SoftServer::new(client);
-            // FIXME
             server.read_command().unwrap();
+            server.send_file("Cargo.toml").unwrap();
+        });
+        let client_stream = net::TcpStream::connect(addr).unwrap();
+        let mut client = SoftClient::new(client_stream);
+        let _data = client.get("Cargo.toml").unwrap();
+        server_thread.join().unwrap();
+    }
+
+    #[test]
+    fn file_transfert_file_stream() {
+        let server_stream = fs::OpenOptions::new()
+            .read(true)
+            .write(true)
+            .create(true)
+            .open(".file_streamt")
+            .unwrap();
+        let client_stream =
+            fs::OpenOptions::new().read(true).write(true).open(".file_streamt").unwrap();
+        let mut server = SoftServer::new(server_stream);
+        let mut client = SoftClient::new(client_stream);
+        let server_thread = thread::spawn(move || {
+            server.read_command().unwrap();
+            server.send_file("Cargo.toml").unwrap();
+        });
+        let _data = client.get("Cargo.toml").unwrap();
+        server_thread.join().unwrap();
+        fs::remove_file(".file_streamt").unwrap();
+    }
+
+    #[test]
+    #[should_panic]
+    fn file_transfert_fail() {
+        let server_stream = net::TcpListener::bind(("0.0.0.0", super::DEFAULT_PORT + 2)).unwrap();
+        let addr = server_stream.local_addr().unwrap();
+        let server_thread = thread::spawn(move || {
+            let (client, _) = server_stream.accept().unwrap();
+            let mut server = SoftServer::new(client);
             server.send_file("Cargo.toml").unwrap();
         });
         let client_stream = net::TcpStream::connect(addr).unwrap();

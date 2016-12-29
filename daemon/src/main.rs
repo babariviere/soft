589d8df966069dd21cd303a612785fc91fb41822
babariviere <babariviere@protonmail.com>
2016-12-29 15:59:49 +0100

    Add a daemon to debug cli

---
+++ daemon/src/main.rs
@@ -0,0 +1,21 @@
+extern crate soft;
+
+use soft::server::SoftServer;
+use std::net::TcpListener;
+
+fn main() {
+    // TODO cli parsing and configuration
+    let mut server = SoftServer::new("soft-daemon", Some(8)).unwrap();
+    server.get_users().add_user("soft", "soft");
+    let listener = TcpListener::bind(("127.0.0.1", soft::DEFAULT_PORT)).unwrap();
+    println!("Listening for client...");
+    for stream in listener.incoming() {
+        println!("New client connected");
+        match stream {
+            Ok(stream) => {
+                server.new_connection(stream);
+            }
+            Err(e) => panic!("{}", e),
+        }
+    }
+}
